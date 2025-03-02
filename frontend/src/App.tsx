a9900c8fd835387e0675a8da564819f1393e97a7
Bikash-Kshetri <kshetridilip247@gmail.com>
2025-03-02 17:24:33 +0545

    Frontend setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className=" bg-amber-200">
+      <h1 className=" text-lg text-center text-red-600 uppercase"> THis is frontend setup</h1>
+    </div>
+  )
+}
+
+export default App
